15a5b032e8299d3ce0a8e7d4cba461cfdb4a0f76
Domos <domos.suszta@hotmail.com>
2022-05-16 15:46:11 +0200

    fix2

--- components/TourDetails.tsx
+++ components/TourDetails.tsx
@@ -15,16 +15,16 @@ const options = {
 
 // Import Swiper styles
 import "swiper/css";
-<<<<<<< HEAD
+
 import "swiper/css/free-mode";
 import "swiper/css/navigation";
 import "swiper/css/thumbs";
 import { PhoneIcon } from "@heroicons/react/outline";
-=======
+
 // import "swiper/css/free-mode";
 // import "swiper/css/navigation";
 // import "swiper/css/thumbs";
->>>>>>> 661f10ec04849b56d0e2fbbfd21bcabee8ea1b96
+
 
 SwiperCore.use([FreeMode, Navigation, Thumbs]);
 const getDayDifferenceBetweenTwoDates = (date1: any, date2: any) => {
@@ -44,7 +44,6 @@ export default function TourDetails({ TourDetails }: any) {
   const [thumbsSwiper, setThumbsSwiper] = useState<any>(null);
 
   return (
-<<<<<<< HEAD
     <div className="  ">
       <div className="p-5 ">
         <section className="text-gray-600 body-font overflow-hidden">
@@ -97,361 +96,46 @@ export default function TourDetails({ TourDetails }: any) {
                   })}
                 </Swiper>
               </div>
-              <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+              <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0 flex flex-col" >
                 <h2 className="text-sm title-font text-gray-500 tracking-widest">
                 {TourDetails.tourCountries}
                 </h2>
-                <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
+                <h1 className=" text-4xl title-font font-bold mb-1 text-orange-500">
                 {TourDetails.tourTitle}
                 </h1>
                 <div className="flex mb-4">
-                  <span className="flex items-center">
-                    <svg
-                      fill="currentColor"
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-4 h-4 text-yellow-500"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                    </svg>
-                    <svg
-                      fill="currentColor"
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-4 h-4 text-yellow-500"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                    </svg>
-                    <svg
-                      fill="currentColor"
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-4 h-4 text-yellow-500"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                    </svg>
-                    <svg
-                      fill="currentColor"
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-4 h-4 text-yellow-500"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                    </svg>
-=======
-    <section className="text-gray-600 body-font overflow-hidden my-28">
-      <div className="container p-4 sm:14 mx-auto border-gray-300 border-2">
-        <div className="lg:w-full  flex flex-wrap ">
-          <div className='w-full sm:w-1/2'>
-
-       
-          <SimpleReactLightbox>
-            <SRLWrapper options={options}>
-              <Swiper
-                loop={true}
-                spaceBetween={0}
-                navigation={true}
-                className="w-full h-full relative"
-              > 
-                {TourDetails.tourPhotos.map((x: any, i: any) => {
-                  return (
-                    <SwiperSlide key={x._id}>
-                      <a href={x}>
-                        <img
-                          className=""
-                          src={x}
-                        />{" "}
-                      </a>
-                    </SwiperSlide>
-                  );
-                })}
-              </Swiper>
-              <Swiper
-          onSwiper={setThumbsSwiper}
-          loop={true}
-          spaceBetween={1}
-          slidesPerView={10}
-          freeMode={true}
-          watchSlidesProgress={true}
-          className="w-full"
-        >
-          {TourDetails.tourPhotos.map((x: any) => {
-            return (
-              <SwiperSlide key={x._id} className=" w-28 min-w-[100px]">
-                <img
-                  className="object-center object-cover min-w-[100px]  sm:w-full min-h-[100px] max-h-[100px]  "
-                  src={x}
-                />
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-            </SRLWrapper>
-          </SimpleReactLightbox>
-          </div>
-      {/* <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/400x400" /> */}
-
-          <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-            <h2 className="text-sm title-font text-gray-500 tracking-widest">
-              BRAND NAME
-            </h2>
-            <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
-              The Catcher in the Rye
-            </h1>
-            <div className="flex mb-4">
-              <span className="flex items-center">
-                <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  className="w-4 h-4 text-yellow-500"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                </svg>
-                <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  className="w-4 h-4 text-yellow-500"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                </svg>
-                <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  className="w-4 h-4 text-yellow-500"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                </svg>
-                <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  className="w-4 h-4 text-yellow-500"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                </svg>
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  className="w-4 h-4 text-yellow-500"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                </svg>
-                <span className="text-gray-600 ml-3">4 Reviews</span>
-              </span>
-              <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
-                <a className="text-gray-500">
-                  <svg
-                    fill="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                  </svg>
-                </a>
-                <a className="text-gray-500">
-                  <svg
-                    fill="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                  </svg>
-                </a>
-                <a className="text-gray-500">
-                  <svg
-                    fill="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                  </svg>
-                </a>
-              </span>
-            </div>
-            <p className="leading-relaxed">
-              Fam locavore kickstarter distillery. Mixtape chillwave tumeric
-              sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo
-              juiceramps cornhole raw denim forage brooklyn. Everyday carry +1
-              seitan poutine tumeric. Gastropub blue bottle austin listicle
-              pour-over, neutra jean shorts keytar banjo tattooed umami
-              cardigan.
-            </p>
-            <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
-              <div className="flex">
-                <span className="mr-3">Color</span>
-                <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-                <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-                <button className="border-2 border-gray-300 ml-1 bg-yellow-500 rounded-full w-6 h-6 focus:outline-none"></button>
-              </div>
-              <div className="flex ml-6 items-center">
-                <span className="mr-3">Size</span>
-                <div className="relative">
-                  <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-yellow-200 focus:border-yellow-500 text-base pl-3 pr-10">
-                    <option>SM</option>
-                    <option>M</option>
-                    <option>L</option>
-                    <option>XL</option>
-                  </select>
-                  <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
->>>>>>> 661f10ec04849b56d0e2fbbfd21bcabee8ea1b96
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-<<<<<<< HEAD
-                      className="w-4 h-4 text-yellow-500"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                    </svg>
-                    <span className="text-gray-600 ml-3">4 Reviews</span>
-                  </span>
               
-                </div>
                 <p className="leading-relaxed">
                  {TourDetails.TourDetails}
                 </p>
-                <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
-               
-                  <div className="flex ml-6 items-center">
-               
-                  </div>
                 </div>
+                <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
+             
+                  <div className="flex ml-6 items-center">
+             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima tempore, nihil pariatur maiores quia laborum quas velit eligendi fugiat placeat neque molestias aperiam, quis perferendis inventore vitae corrupti accusamus esse?
+                 lorem <br></br>
+                 <br></br>  Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam reiciendis facilis odit facere maiores vitae, magnam commodi fuga culpa tenetur laborum modi laboriosam et quis libero nesciunt hic quo soluta. </div>
+                </div>
+
+
+                  
+
                 <div className="flex justify-between">
                   <span className="title-font font-medium text-2xl text-gray-900">
                   {formatter.format(TourDetails.priceFrom)} Ft
                   </span>
-                
-           
+                  <a href='tel:+36 50 117 1026' className="flex items-center justify-center p-4 border-2 border-orange-300 text-white text-3xl rounded-br-2xl bg-orange-400 hover:text-white hover:scale-125 transition cursor-pointer">+36 50 117 10 26</a>
+
                 </div>
               </div>
             </div>
           </div>
+         
+     
         </section>
-=======
-                      className="w-4 h-4"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M6 9l6 6 6-6"></path>
-                    </svg>
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className="flex">
-              <span className="title-font font-medium text-2xl text-gray-900">
-                $58.00
-              </span>
-              <button className="flex ml-auto text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded">
-                Button
-              </button>
-              <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                <svg
-                  fill="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                </svg>
-              </button>
-            </div>
-          </div>
+
         </div>
->>>>>>> 661f10ec04849b56d0e2fbbfd21bcabee8ea1b96
-      </div>
-    </section>
-    // <div className="sm:m-20 sm:mx-40 ">
-    //   <div className="p-5">
-    //     <SimpleReactLightbox>
-    //       <SRLWrapper options={options}>
-    //         <Swiper
-    //           loop={true}
-    //           spaceBetween={10}
-    //           navigation={true}
-    //           thumbs={{ swiper: thumbsSwiper }}
-    //           className="w-2/3"
-    //         >
-    //           {TourDetails.tourPhotos.map((x: any, i: any) => {
-    //             return (
-    //               <SwiperSlide key={x._id}>
-    //                 <a href={x}>
-    //                   <img
-    //                     className="object-center object-cover w-full max-h-[500px] "
-    //                     src={x}
-    //                   />{" "}
-    //                 </a>
-    //               </SwiperSlide>
-    //             );
-    //           })}
-    //         </Swiper>
-    //       </SRLWrapper>
-    //     </SimpleReactLightbox>
-    //     <Swiper
-    //       onSwiper={setThumbsSwiper}
-    //       loop={true}
-    //       spaceBetween={1}
-    //       slidesPerView={10}
-    //       freeMode={true}
-    //       watchSlidesProgress={true}
-    //       className="w-2/3"
-    //     >
-    //       {TourDetails.tourPhotos.map((x: any) => {
-    //         return (
-    //           <SwiperSlide key={x._id} className=" w-28 min-w-[100px]">
-    //             <img
-    //               className="object-center object-cover min-w-[100px]  sm:w-full min-h-[100px] max-h-[100px]  "
-    //               src={x}
-    //             />
-    //           </SwiperSlide>
-    //         );
-    //       })}
-    //     </Swiper>
-    //   </div>
-    // </div>
+     </div>
+  
   );
 }
